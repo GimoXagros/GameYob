e8518aed00432c1750c81b4520e84afc1eda7413
Xagros <30635204+GimoXagros@users.noreply.github.com>
2026-08-08 17:16:26 +0900

    feat: report v0.5.5 build details

--- platform/common/version.cpp
+++ platform/common/version.cpp
@@ -1,7 +1,30 @@
 #include <stdio.h>
 #include "console.h"
+#include "nifi_protocol.h"
+
+#ifndef GIT_REVISION
+#define GIT_REVISION "unknown"
+#endif
+
+#ifdef DS
+extern bool __dsimode;
+#endif
 
 void printVersionInfo() {
     clearConsole();
-    printf("GameYob %s\n", VERSION_STRING);
+    printf("GameYob v0.5.5-ko\n");
+    printf("Revision: %s\n", GIT_REVISION);
+#ifdef DS
+    printf("Target: %s\n", __dsimode ? "Nintendo DSi mode" :
+            "Nintendo DS mode");
+    printf("Framework: BlocksDS 1.22.2\n");
+#elif defined(_3DS)
+    printf("Target: Nintendo 3DS (native)\n");
+    printf("Framework: libctru/devkitARM\n");
+#elif defined(SDL)
+    printf("Target: SDL test build\n");
+#else
+    printf("Target: unknown\n");
+#endif
+    printf("Link protocol: v%d\n", nifi::PROTOCOL_VERSION);
 }
